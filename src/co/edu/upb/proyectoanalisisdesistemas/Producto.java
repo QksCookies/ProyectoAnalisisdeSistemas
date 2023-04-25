239b73df591425871add5186e1f472fd5cb97451
Juan David Muñoz Rojas <129176000+JHAYCO9@users.noreply.github.com>
2023-04-24 22:53:32 -0500

    Update Producto.java
    agregue un método para poder obtener descuento en los productos.

--- src/co/edu/upb/proyectoanalisisdesistemas/Producto.java
+++ src/co/edu/upb/proyectoanalisisdesistemas/Producto.java
@@ -5,12 +5,14 @@ public class Producto {
     private String descripcion;
     private double precio;
     private int cantidadDisponible;
+    private double descuento;
 
     public Producto(String nombre, String descripcion, double precio, int cantidadDisponible) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.cantidadDisponible = cantidadDisponible;
+	this.descuento = descuento;
     }
 
     public String getNombre() {
@@ -33,8 +35,22 @@ public class Producto {
         this.cantidadDisponible = cantidadDisponible;
     }
     
+    public double getDescuento() {
+        return descuento;
+    }
+	
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+	
+    public double aplicarDescuento() {
+        double descuentoAplicado = precio * descuento;
+        double precioConDescuento = precio - descuentoAplicado;
+        return precioConDescuento;
+    }	
+	
     public double calcularPrecioTotalProducto() {
-        return precio * cantidadDisponible;
+        return precio * cantidadDisponible + descuento;
     }
 
 	public void restarCantidad(int cantidadDisponible2) {
